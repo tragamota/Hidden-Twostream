90b93dd9021bf310169476d60fbdfdca0bbfbf0b
Ian van de Poll <ian.vd.poll@gmail.com>
2024-06-24 15:40:43 +0200

    Fix UFC101 dataset loading

--- datasets/UFC101.py
+++ datasets/UFC101.py
@@ -1,19 +1,19 @@
 import os
 
 import numpy as np
-import pandas as pd
 import torch
+
 from torch.utils.data import Dataset
 from torchvision.io import read_video
 
-def _pick_frame_even(video):
+def _pick_frame_even(video, transform):
     T, C, H, W = video.size()
 
-    frames = torch.zeros(33, H, W, dtype=torch.float)
-    frame_numbers = np.linspace(0, T, 11)
+    frames = torch.zeros(33, 224, 224, dtype=torch.float)
+    frame_numbers = np.linspace(0, T - 1, 11, dtype=int)
 
     for idx, frame_number in enumerate(frame_numbers):
-        frames[idx*3:idx*3+3, :, :, :] = video[frame_number, :, :, :]
+        frames[idx*3:idx*3+3, :, :] = transform(video[frame_number, :, :, :])
 
     return frames
 
@@ -21,7 +21,7 @@ def _pick_frame_even(video):
 class UFC101(Dataset):
 
     def __init__(self, annotations, root_dir, transform=None):
-        self.items = pd.read_csv(annotations)
+        self.items = annotations
         self.dir = root_dir
         self.transform = transform
 
@@ -30,13 +30,9 @@ class UFC101(Dataset):
 
     def __getitem__(self, idx):
         video_path = os.path.join(self.dir, self.items.iloc[idx, 0])
-        label = self.img_labels.iloc[idx, 2]
+        label = self.items.iloc[idx, 1] - 1
 
-        video = read_video(video_path, output_format="TCHW")
-        frames = _pick_frame_even(video)
-
-        for i in range(frames.size()[0] // 3):
-            if self.transform:
-                frames[i * 3:i * 3 + 3, :, :] = self.transform(frames[i * 3: i * 3 + 3, :, :])
+        video, _, _ = read_video(video_path, output_format="TCHW", pts_unit='sec')
+        frames = _pick_frame_even(video, self.transform)
 
         return frames, label
