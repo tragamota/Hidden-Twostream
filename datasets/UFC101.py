815db4007a1a4f53fe3167fe9165a1f210e0b4e8
Ian van de Poll <ian.vd.poll@gmail.com>
2024-12-21 18:01:01 +0100

    Make UCF-101 dataset load the preprocessed dataset

--- datasets/UFC101.py
+++ datasets/UFC101.py
@@ -1,21 +1,7 @@
 import os
 
-import numpy as np
-import torch
-
 from torch.utils.data import Dataset
 from torchvision.io import read_video
-
-def _pick_frame_even(video, transform):
-    T, C, H, W = video.size()
-
-    frames = torch.zeros(33, 224, 224, dtype=torch.float)
-    frame_numbers = np.linspace(0, T - 1, 11, dtype=int)
-
-    for idx, frame_number in enumerate(frame_numbers):
-        frames[idx*3:idx*3+3, :, :] = transform(video[frame_number, :, :, :])
-
-    return frames
 
 
 class UFC101(Dataset):
@@ -32,7 +18,15 @@ class UFC101(Dataset):
         video_path = os.path.join(self.dir, self.items.iloc[idx, 0])
         label = self.items.iloc[idx, 1] - 1
 
-        video, _, _ = read_video(video_path, output_format="TCHW", pts_unit='sec')
-        frames = _pick_frame_even(video, self.transform)
+        video, _, _ = read_video(video_path, output_format="THWC", pts_unit='sec')
+        video = video.numpy()
 
-        return frames, label
+        if self.transform is not None:
+            video = self.transform(images=video)["images"]
+
+        T, C, H, W = video.shape
+
+        video = video.reshape(T * C, H, W)
+
+
+        return video, label
